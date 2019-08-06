dfd44aaca95f67d4230b9425ac937f815530b330
taeiim <ti535666@gmail.com>
2019-08-07 00:46:18 +0900

    Anko 추가

--- Plock/app/src/main/java/com/teamnexters/plock/ui/main/MainActivity.kt
+++ Plock/app/src/main/java/com/teamnexters/plock/ui/main/MainActivity.kt
@@ -3,11 +3,17 @@ package com.teamnexters.plock.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.teamnexters.plock.R
+import com.teamnexters.plock.ui.show.ShowActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        writeCardLayout.setOnClickListener {  }
+        showCardLayout.setOnClickListener { startActivity<ShowActivity>() }
     }
 }
