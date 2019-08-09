7dfb3524e9be5f5d2607118c9bf16e3bc28cf237
taeiim <ti535666@gmail.com>
2019-08-09 23:27:53 +0900

    Anko 제거/Extensions 추가

---
+++ Plock/app/src/main/java/com/teamnexters/plock/ui/writeCard/WriteCardActivity.kt
@@ -0,0 +1,21 @@
+package com.teamnexters.plock.ui.writeCard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.teamnexters.plock.R
+import kotlinx.android.synthetic.main.toolbar_custom.*
+
+class WriteCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_card)
+        initToolbar()
+
+    }
+
+    private fun initToolbar() {
+        tv_toolbar_center.text = "작성"
+        imv_toolbar_right.setImageResource(R.drawable.toolbar_next)
+    }
+
+}
