26662bd7f8cd0e5603fca9cde709959430f8c025
taeiim <ti535666@gmail.com>
2019-08-11 04:30:43 +0900

    카드 Flip Animation 추가

--- Plock/app/src/main/java/com/teamnexters/plock/ui/writecard/WriteCardActivity.kt
+++ Plock/app/src/main/java/com/teamnexters/plock/ui/writecard/WriteCardActivity.kt
@@ -7,16 +7,20 @@ import com.teamnexters.plock.R
 import com.teamnexters.plock.data.provideTimeCapsuleDao
 import kotlinx.android.synthetic.main.activity_write_card.*
 import kotlinx.android.synthetic.main.toolbar_custom.*
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 
 
 class WriteCardActivity : AppCompatActivity() {
-    private var isFinalLevel = false
-
     internal val viewModelFactory by lazy {
         WriteCardViewModelFactory(provideTimeCapsuleDao(this))
     }
 
     lateinit var viewModel: WriteCardViewModel
+
+    private lateinit var rightOutAnim: AnimatorSet
+    private lateinit var leftInAnim: AnimatorSet
+    private var isBackVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +31,57 @@ class WriteCardActivity : AppCompatActivity() {
         )[WriteCardViewModel::class.java]
 
         initToolbar()
+        loadFlipAnimations()
 
+        imv_toolbar_left.setOnClickListener {
+            flipToFront()
+            setToolbarRightBtnNext()
+        }
+
+        imv_toolbar_right.setOnClickListener {
+            if (!isBackVisible) {
+                flipToBack()
+                setToolbarRightBtnFinish()
+            } else {
+                showFinalCheckDialog()
+            }
+        }
+    }
+
+    private fun showFinalCheckDialog() {
+
+    }
+
+    private fun flipToBack(){
+        rightOutAnim.setTarget(cardFront)
+        leftInAnim.setTarget(cardBack)
+        isBackVisible = true
+        startAnim()
+    }
+
+    private fun flipToFront(){
+        rightOutAnim.setTarget(cardBack)
+        leftInAnim.setTarget(cardFront)
+        isBackVisible = false
+        startAnim()
+    }
+
+    private fun loadFlipAnimations() {
+        rightOutAnim = AnimatorInflater.loadAnimator(this, R.animator.anim_flip_right_out) as AnimatorSet
+        leftInAnim = AnimatorInflater.loadAnimator(this, R.animator.anim_flip_left_in) as AnimatorSet
+        changeCameraDistance()
+    }
+
+    private fun changeCameraDistance() {
+        val distance = 8000
+        val scale = resources.displayMetrics.density * distance
+        cardFront.cameraDistance = scale
+        cardBack.cameraDistance = scale
+    }
+
+    private fun startAnim(){
+        rightOutAnim.start()
+        leftInAnim.start()
     }
 
     private fun setToolbarRightBtnNext() =
