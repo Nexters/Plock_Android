0f2ae13c0eb2a404dd1485e8a0288917d16c5688
Kyudong3 <kyudong3@gmail.com>
2019-09-17 19:43:35 +0900

    위치 권한 클래스로 분리

--- Plock/app/src/main/java/com/teamnexters/plock/util/CheckLocationPermission.kt
+++ Plock/app/src/main/java/com/teamnexters/plock/util/CheckLocationPermission.kt
@@ -3,32 +3,27 @@ package com.teamnexters.plock.util
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.view.View
+import android.content.Intent
 import android.widget.Toast
-import com.google.android.gms.maps.GoogleMap
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
 import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
+import com.teamnexters.plock.ui.write.MapLocationActivity
 
 // 앱 내 위치 권한
 class CheckLocationPermission {
     companion object {
-        fun checkPermission(activity: Activity, mMap: GoogleMap, btn: View?) {
+        fun checkPermission(activity: Activity) {
             Dexter.withActivity(activity)
                 .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
                 .withListener(object : PermissionListener {
                     @SuppressLint("MissingPermission")
                     override fun onPermissionGranted(response: PermissionGrantedResponse?) {
                         Toast.makeText(activity, "Permission Granted!!", Toast.LENGTH_SHORT).show()
-                        if (btn != null) {
-                            mMap.isMyLocationEnabled = true
-                            btn.visibility = View.GONE
-                        } else {
-                            mMap.isMyLocationEnabled = false
-                        }
+                        (activity as MapLocationActivity).getDeviceLocation()
                     }
 
                     override fun onPermissionRationaleShouldBeShown(
@@ -39,6 +34,11 @@ class CheckLocationPermission {
                     }
 
                     override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                        val intent = Intent()
+                        intent.putExtra("lat", 0.0)
+                        intent.putExtra("long", 0.0)
+                        intent.putExtra("location", "")
+                        activity.setResult(100, intent)
                         activity.finish()
                         Toast.makeText(activity, "앱 내 위치권한을 확인하세요", Toast.LENGTH_SHORT).show()
                     }
