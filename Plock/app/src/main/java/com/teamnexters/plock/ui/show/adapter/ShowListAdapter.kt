2cc7a97d1ab1c8900bc0564008c3fff0905ec91c
Kyudong3 <kyudong3@gmail.com>
2019-09-17 19:42:40 +0900

    리스트 어댑터 거리 계산 추가

--- Plock/app/src/main/java/com/teamnexters/plock/ui/show/adapter/ShowListAdapter.kt
+++ Plock/app/src/main/java/com/teamnexters/plock/ui/show/adapter/ShowListAdapter.kt
@@ -1,7 +1,6 @@
 package com.teamnexters.plock.ui.show.adapter
 
 import android.content.Context
-import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.view.LayoutInflater
@@ -13,46 +12,55 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.teamnexters.plock.R
 import com.teamnexters.plock.data.entity.TimeCapsule
-import com.teamnexters.plock.ui.detailcard.DetailCardActivity
 import com.teamnexters.plock.ui.show.ShowListFragment
-import com.teamnexters.plock.ui.show.model.Location
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ShowListAdapter(
-    private val itemsMap: LinkedHashMap<Location, ArrayList<TimeCapsule>>,
+    private val itemsMap: ArrayList<TimeCapsule>,
     private val context: Context?,
     private val lastLocation: android.location.Location,
     private val fragment: ShowListFragment
 ) : RecyclerView.Adapter<ShowListAdapter.ListViewHolder>() {
 
-    private var keys = itemsMap.keys
+    var itemClick: ItemClick? = null
+
+    interface ItemClick {
+        fun onClick(view: View, mapPosition: TimeCapsule)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         return ListViewHolder(LayoutInflater.from(context).inflate(R.layout.show_list_item, parent, false))
     }
 
     override fun getItemCount(): Int {
-        return itemsMap.keys.size
-    }
-
-    private fun getKey(position: Int): Location {
-        return keys.elementAt(position)
+        return itemsMap.size
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        val location = getKey(position)
-        holder.bindList(itemsMap[location])
+        if (itemClick != null) {
+            holder.itemView.setOnClickListener { v ->
+                itemClick?.onClick(v, itemsMap[position])
+            }
+        }
+        val item = itemsMap[position]
+        holder.apply {
+            bindList(item)
+        }
     }
 
-    fun removeItem(index: Location?) {
-        val a =itemsMap.keys.indexOf(index)
-        itemsMap.remove(index)
-        notifyItemRemoved(a)
-        notifyItemRangeChanged(a, itemsMap.keys.size)
+    fun removeItem(index: Int) {
+        itemsMap.removeAt(index)
+        notifyItemRemoved(index)
+        notifyItemRangeChanged(index, itemsMap.size)
+        if (itemsMap.isEmpty()) {
+            fragment.changeView()
+        }
     }
 
     inner class ListViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+
+        val view = itemView
 
         val parentLayout = itemView?.findViewById<ConstraintLayout>(R.id.parent_layout_list_item)
         val date = itemView?.findViewById<AppCompatTextView>(R.id.txv_date_show_list)
@@ -60,20 +68,19 @@ class ShowListAdapter(
         val photo = itemView?.findViewById<AppCompatImageView>(R.id.imv_picture_show_list)
         val lock_icon = itemView?.findViewById<AppCompatImageView>(R.id.imv_lock_show_list)
 
-        fun bindList(arrayList: ArrayList<TimeCapsule>?) {
+        fun bindList(capsule: TimeCapsule?) {
             photo?.setImageBitmap(
                 BitmapFactory.decodeByteArray(
-                    arrayList?.get(0)?.photo,
+                    capsule?.photo,
                     0,
-                    arrayList?.get(0)?.photo!!.size
+                    capsule?.photo!!.size
                 )
             )
-            date?.text = getDateStr(arrayList?.get(0)?.date!!)
-            title?.text = arrayList[0].title
+            date?.text = getDateStr(capsule?.date!!)
+            title?.text = capsule.title
+
             // 위치 비교해서 lock, unlock 구분
-            if (((arrayList[0].latitude < lastLocation.latitude + 0.01) && (arrayList[0].longitude < lastLocation.longitude + 0.01)
-                        && (arrayList[0].latitude > lastLocation.latitude - 0.01) && (arrayList[0].longitude > lastLocation.longitude - 0.01))
-            ) {
+            if (calculateDistance(capsule.latitude, capsule.longitude, lastLocation.latitude, lastLocation.longitude) < 100) {
                 parentLayout?.setBackgroundColor(Color.TRANSPARENT)
                 lock_icon?.visibility = View.INVISIBLE
                 itemView.isClickable = true
@@ -82,20 +89,18 @@ class ShowListAdapter(
                 lock_icon?.visibility = View.VISIBLE
                 itemView.isClickable = false
             }
-
-            if (!itemView.isClickable)
-            else {
-                itemView.setOnClickListener {
-                    val intent = Intent(it.context, DetailCardActivity::class.java)
-                    intent.putExtra("list", arrayList)
-                    fragment.startActivityForResult(intent, 5)
-                }
-            }
-
         }
     }
 
     private fun getDateStr(date: Date): String {
         return SimpleDateFormat("yyyy.MM.dd").format(date)
     }
+
+    private fun calculateDistance(lat1: Double, lng1: Double, lat2: Double, lng2: Double): Float {
+        val results = FloatArray(1)
+        android.location.Location.distanceBetween(lat1, lng1, lat2, lng2, results)
+        // distance in meter
+        return results[0]
+    }
+
 }
